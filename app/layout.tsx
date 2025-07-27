dc5425ff60547f3c7151ee1b587015943d56386b
NGALEU Stephane <ngaleusteph@gmail.com>
2025-07-27 10:03:13 +0100

    remove v0 mark

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Casuk',
+  description: 'Casuk - Connecting and supporting Cameroonian communities across the United Kingdom through culture, events, and resources.',
+  generator: 'casuk',
 }
 
 export default function RootLayout({
